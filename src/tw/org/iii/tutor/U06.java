b26257e118e5e0f7cfca8a46deeb1a4069c7beab
you <you@LAPTOP-7IF89ASP>
2023-05-02 22:59:39 +0800

    111

---
+++ src/tw/org/iii/tutor/U06.java
@@ -0,0 +1,10 @@
+package tw.org.iii.tutor;
+
+public class U06 {
+	public static void main(String[] args) {
+		boolean v1 =true ;
+		boolean v2 = false;
+		System.out.println(v1);
+		System.out.println(v2);
+	}
+}
